f4e7da3bdf7e7601ae39c7f3a9f2386c97f0cafe
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2025-01-26 19:03:32 -0600

    feat: implement article update functionality by slug with ownership check

--- internal/server/modules/articles/main.go
+++ internal/server/modules/articles/main.go
@@ -1,6 +1,7 @@
 package articles
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/darwin-luque/codespark-go-sqlc-api/domain"
@@ -32,5 +33,48 @@ func (am *ArticlesModule) RegisterRoutes(baseRouter *mux.Router) {
 	{
 		authenticatedRouter.HandleFunc("", am.createArticle()).Methods(http.MethodPost)
 		authenticatedRouter.HandleFunc("", am.listArticles()).Methods(http.MethodGet)
+	}
+
+	blogOwnerRouter := authenticatedRouter.PathPrefix("").Subrouter()
+	blogOwnerRouter.Use(am.checkBlogOwnership("slug"))
+	{
+		blogOwnerRouter.HandleFunc("/{slug}", am.updateArticleBySlug()).Methods(http.MethodPut)
+	}
+}
+
+func (am *ArticlesModule) checkBlogOwnership(whereToCheck string) func(http.Handler) http.Handler {
+	q := domain.New(am.db)
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			user, ok := req.Context().Value(middlewares.USER_KEY).(*domain.User)
+
+			if !ok {
+				utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("invalid user context"))
+				return
+			}
+			var article domain.Article
+
+			switch whereToCheck {
+			case "slug":
+				slug := mux.Vars(req)["slug"]
+				foundArticle, err := q.GetArticleBySlug(req.Context(), slug)
+
+				if err != nil {
+					utils.NotFoundError(w, utils.ErrorM{})
+					return
+				}
+
+				article = foundArticle
+			default:
+				utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("invalid whereToCheck"))
+			}
+
+			if article.AuthorID != user.ID {
+				utils.ErrorResponse(w, http.StatusForbidden, errors.New("forbidden"))
+				return
+			}
+
+			next.ServeHTTP(w, req)
+		})
 	}
 }
---
+++ internal/server/modules/articles/update.go
@@ -0,0 +1,65 @@
+package articles
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/darwin-luque/codespark-go-sqlc-api/domain"
+	"github.com/darwin-luque/codespark-go-sqlc-api/internal/common/utils"
+	"github.com/gorilla/mux"
+)
+
+func (am *ArticlesModule) updateArticleBySlug() http.HandlerFunc {
+	type Input struct {
+		Title       *string `json:"title"`
+		Body        *string `json:"content"`
+		Description *string `json:"description"`
+		Status      *string `json:"status"`
+	}
+
+	q := domain.New(am.db)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		var slug string
+		if foundSlug, exists := vars["slug"]; exists {
+			slug = foundSlug
+		} else {
+			utils.ErrorResponse(w, http.StatusBadRequest, errors.New("missing slug"))
+			return
+		}
+
+		input := &Input{}
+		if err := utils.ReadJSON(req.Body, input); err != nil {
+			utils.ErrorResponse(w, http.StatusBadRequest, err)
+			return
+		}
+
+		if err := utils.Validate.Struct(input); err != nil {
+			utils.ValidationError(w, err)
+			return
+		}
+
+		params := domain.UpdateArticleParams{
+			Title:       input.Title,
+			Body:        input.Body,
+			Description: input.Description,
+			Status:      input.Status,
+			Slug:        slug,
+		}
+
+		article, err := q.UpdateArticle(req.Context(), params)
+
+		if err != nil {
+			switch {
+			case err.Error() == "not found":
+				utils.ErrorResponse(w, http.StatusNotFound, err)
+			default:
+				utils.ErrorResponse(w, http.StatusInternalServerError, err)
+			}
+			return
+		}
+
+		utils.WriteJSON(w, http.StatusOK, utils.M{"article": article})
+	})
+}
