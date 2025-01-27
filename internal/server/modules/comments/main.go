021a3f24676fed1b0739c750fda1bbbd1ed9ff4d
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2025-01-26 19:51:48 -0600

    fix: add comment ownership verification for delete operation

--- internal/server/modules/comments/main.go
+++ internal/server/modules/comments/main.go
@@ -1,12 +1,14 @@
 package comments
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/darwin-luque/codespark-go-sqlc-api/domain"
 	"github.com/darwin-luque/codespark-go-sqlc-api/internal/common/config"
 	"github.com/darwin-luque/codespark-go-sqlc-api/internal/common/utils"
 	"github.com/darwin-luque/codespark-go-sqlc-api/internal/infrastructure/middlewares"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -25,19 +27,57 @@ func New(db domain.DBTX, cfg config.Interface, m *middlewares.Middlewares) *Comm
 }
 
 func (cm *CommentsModule) RegisterRoutes(baseRouter *mux.Router) {
-	articleBasedCommentsRouter := baseRouter.Path("/articles/{slug}/comments").Subrouter()
-	commentsRouter := baseRouter.Path("/comments").Subrouter()
+	withAuth := baseRouter.PathPrefix("").Subrouter()
+	withAuth.Use(cm.m.Authenticate(utils.RequiredAuth))
 
-	authenticatedArticleBasedCommentsRouter := articleBasedCommentsRouter.PathPrefix("").Subrouter()
-	authenticatedArticleBasedCommentsRouter.Use(cm.m.Authenticate(utils.RequiredAuth))
+	articleBasedCommentsRouter := withAuth.Path("/articles/{slug}/comments").Subrouter()
 	{
-		authenticatedArticleBasedCommentsRouter.HandleFunc("", cm.add()).Methods(http.MethodPost)
-		authenticatedArticleBasedCommentsRouter.HandleFunc("", cm.listForArticle()).Methods(http.MethodGet)
+		articleBasedCommentsRouter.HandleFunc("", cm.add()).Methods(http.MethodPost)
+		articleBasedCommentsRouter.HandleFunc("", cm.listForArticle()).Methods(http.MethodGet)
 	}
 
-	authenticatedRouter := commentsRouter.PathPrefix("").Subrouter()
-	authenticatedRouter.Use(cm.m.Authenticate(utils.RequiredAuth))
+	withIsCommentOwner := withAuth.PathPrefix("/comments/{id}").Subrouter()
+	withIsCommentOwner.Use(cm.checkCommentOwnership())
 	{
-		authenticatedRouter.HandleFunc("/{id}", cm.delete()).Methods(http.MethodDelete)
+		withIsCommentOwner.HandleFunc("", cm.delete()).Methods(http.MethodDelete)
+	}
+}
+
+func (cm *CommentsModule) checkCommentOwnership() func(http.Handler) http.Handler {
+	q := domain.New(cm.db)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			commentID := vars["id"]
+
+			commentUUID, err := uuid.Parse(commentID)
+
+			if err != nil {
+				http.Error(w, "Invalid comment ID", http.StatusBadRequest)
+				return
+			}
+
+			user, ok := r.Context().Value(middlewares.USER_KEY).(*domain.User)
+
+			if !ok {
+				utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("invalid user context"))
+				return
+			}
+
+			comment, err := q.GetComment(r.Context(), commentUUID)
+
+			if err != nil {
+				utils.ErrorResponse(w, http.StatusInternalServerError, err)
+				return
+			}
+
+			if comment.AuthorID != user.ID {
+				utils.ErrorResponse(w, http.StatusForbidden, errors.New("unauthorized"))
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
 	}
 }
